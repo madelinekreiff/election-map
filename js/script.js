eb47039d12b15757e38874b59b93172ae3d99dd6
madelinekreiff <madelinekreiff@gmail.com>
2021-07-10 16:53:30 -0700

    Created multiple candidate objects using factory functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-const politician = {
-    name: "",
-    electionResults: null,
-    totalVotes: 0
-};
+// factory function to create new candidates
+const createCandidate = function(name, electionResults, totalVotes) {
+  const politician = {
+    name: name,
+    electionResults: electionResults,
+    totalVotes: totalVotes
+  };
+  return politician;
+}
+
+// create candidates
+const maddieReiff = createCandidate("Maddie Reiff", null, 332);
+const lilyRose = createCandidate("Lily Rose", null, 206);
+
