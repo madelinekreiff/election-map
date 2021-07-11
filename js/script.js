b870416d09d1ca2510d8084bb9c19bd2b2982858
madelinekreiff <madelinekreiff@gmail.com>
2021-07-10 17:15:40 -0700

    Created election results array for each candidate and counted their total number of votes and created an array for all of the state names

--- js/script.js
+++ js/script.js
@@ -8,7 +8,21 @@ const createCandidate = function(name, electionResults, totalVotes) {
   return politician;
 }
 
-// create candidates
-const maddieReiff = createCandidate("Maddie Reiff", null, 332);
-const lilyRose = createCandidate("Lily Rose", null, 206);
+// create election results and array of state names
+const mrResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
+const lrResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
+const stateNames = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
 
+// total votes per candidate
+let mrTotalVotes = 0;
+let lrTotalVotes = 0;
+for (let i of mrResults) {
+    mrTotalVotes += i;
+}
+for (let i of lrResults) {
+    lrTotalVotes += i;
+}
+
+// create candidates
+const maddieReiff = createCandidate("Maddie Reiff", mrResults, mrTotalVotes);
+const lilyRose = createCandidate("Lily Rose", lrResults, lrTotalVotes);
