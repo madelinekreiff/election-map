f7780396fbd0230de6e98c7c0033f865d5c10d47
madelinekreiff <madelinekreiff@gmail.com>
2021-07-10 17:32:18 -0700

    Deleted for loop to count total votes and added a method in the createCandidate factory function to count total votes there instead

--- js/script.js
+++ js/script.js
@@ -3,8 +3,14 @@ const createCandidate = function(name, electionResults, totalVotes) {
   const politician = {
     name: name,
     electionResults: electionResults,
-    totalVotes: totalVotes
-  };
+    totalVotes: function () {
+        this.totalVotes = 0;
+        for (let i of this.electionResults) {
+            this.totalVotes += i;
+        }
+    }
+  }; // end object
+  politician.totalVotes();
   return politician;
 }
 
@@ -13,16 +19,8 @@ const mrResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 
 const lrResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
 const stateNames = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
 
-// total votes per candidate
-let mrTotalVotes = 0;
-let lrTotalVotes = 0;
-for (let i of mrResults) {
-    mrTotalVotes += i;
-}
-for (let i of lrResults) {
-    lrTotalVotes += i;
-}
-
 // create candidates
-const maddieReiff = createCandidate("Maddie Reiff", mrResults, mrTotalVotes);
-const lilyRose = createCandidate("Lily Rose", lrResults, lrTotalVotes);
+const maddieReiff = createCandidate("Maddie Reiff", mrResults);
+const lilyRose = createCandidate("Lily Rose", lrResults);
+
+console.log(maddieReiff, lilyRose);
